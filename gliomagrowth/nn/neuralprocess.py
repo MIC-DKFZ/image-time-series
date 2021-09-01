89a8ac423c806fc276f0df6ed4b1c317926a6dd6
Jens Petersen <jens.petersen@dkfz.de>
2021-09-01 15:29:29 +0200

    Remove debug code

--- gliomagrowth/nn/neuralprocess.py
+++ gliomagrowth/nn/neuralprocess.py
@@ -269,13 +269,7 @@ class ImageProcess(nn.Module):
         else:
             prior = representation[-1]
         prior = prior.mean(1, keepdim=True)
-        try:
-            prior = tensor_to_loc_scale(prior, torch.distributions.Normal, axis=2)
-        except ValueError as e:
-            print(prior.shape)
-            print(torch.any(torch.isnan(prior)))
-            print(prior)
-            raise e
+        prior = tensor_to_loc_scale(prior, torch.distributions.Normal, axis=2)
         if save:
             self.prior = prior
 
